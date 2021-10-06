27b9a27b77d20c56c288c800ebc5b278d7172681
mpcabete <mateus.cabete@usp.br>
2021-10-05 22:21:34 -0300

    hash comparation, multi page scraping, csv file saving

--- README.md
+++ README.md
@@ -8,9 +8,13 @@
 - [x] programatically fetch API data from IO
 - [x] parse data into a CSV format
 - [x] insert price data from IO to tools data
-- [ ] test using the NFT hash as unique ID
-- [ ] quick refactor
-- [ ] implement automatic scraping for more than one page
+- [x] test using the NFT hash as unique ID
+- [x] quick refactor
+- [x] implement automatic scraping for more than one page
+- [x] automatically save CSV file
+- [ ] dinamically set the number of pages to be scraped
+- [ ] optimize price hash matching script
+- [ ] check the "available" field in the cnftIo response
 - [ ] error handle API calls
 
 
--- index.js
+++ index.js
@@ -1,61 +1,120 @@
 import fetch from 'node-fetch';
 import {csvFormat} from 'd3-dsv';
-const project = 'yummiuniverse'
-const page = 1
+import { writeFile } from 'fs/promises';
+
+class Asset{
+  // constructor(name, id, price, hash, rarityScore, rarityRank){
+  constructor(cnftToolsItem){
+    this.name = cnftToolsItem.name
+    this.id = cnftToolsItem.assetID
+    this.price = cnftToolsItem.price
+    this.hash = cnftToolsItem.url
+    this.rarityScore = cnftToolsItem.rarityScore
+    this.rarityRank = cnftToolsItem.rarityRank
+  }
+}
 
 async function main(){
 
-  const cnftToolsData = await cnftTools()
-  const ioData = await cnftIo()
-  // console.log(JSON.stringify(ioData.assets,null,4))
-  const prices = getPrices(ioData)
-  const mergedData = cnftToolsData.stats.map(injectPrice)
-  // console.log('cnftToolsData',csvFormat(mergedData))
+  const toolsData = await getCnftToolsData('yummiuniverse')
+  const collection = toolsData.map(item => new Asset(item))
+  // console.log('collection',collection)
 
-  function injectPrice(obj){
-    const objHash = obj.url
-    const price = prices.find(x=>{
-      console.log(x.hash,objHash)
-      return x.hash === objHash
-    })
-    console.log('price',price)
-    obj.price = price 
-    return obj
+  const ioData = await getCnftIoData('Yummi Universe - Naru')
+  const hashsAndPricesMap = getPrices(ioData)
+
+  collection.forEach(asset => injectPrice(asset, hashsAndPricesMap))
+  // console.log('ioData',collection)
+  // const ioData = await getCnftIoData('Clay Nation by Clay Mates', 1)
+  // console.log('ioData',ioData
+  // console.log(JSON.stringify(ioData,null,4))
+  //
+  // const prices = getPrices(ioData)
+  // const mergedData = cnftToolsData.map(injectPrice)
+  const results = csvFormat(collection)
+
+  try {
+    await writeFile('data.csv',results);
+    console.log('successfully saved data.csv');
+  } catch (error) {
+    console.error('there was an error:', error.message);
   }
+  }
+main()
+  
+
+  function injectPrice(asset,map){
+    const match = map.find(x=>{
+      return x.hash === asset.hash 
+    })
+    if(match){
+      
+      console.log(`${asset.name} price found: ${match.price}`)
+      asset.price = match.price 
+    }else{
+      asset.price = null
+    }
+    return asset
+  }
+
   function getPrices(ioData){
-    return ioData.assets.map(x=>{
+    return ioData.map(x=>{
         return { hash:x.metadata.thumbnail.slice(-46) , price:x.price }
-        
+
       })
   }
 
-  async function cnftTools(){
-    const uri = `https://cnft.tools/api/${project}?background=x&body=x&face=x&headwear=x&sort=ASC&method=rarity&page=${page}&`
-    const response = await fetch(uri);
-    const data = await response.json();
+  async function getCnftToolsData(project, n=15){
+    const data = []
+    let i = 1 
+    while (i <= n){
+      const page = await getCnftToolsPage(project, i)
+      console.log('cnftTools page '+ i ,page.length)
+      data.push(...page)
+      i++
+    }
+    console.log(`cnftools ${n} pages: ${data.length} items`)
     return data
   }
 
-  async function cnftIo(){
+async function getCnftIoData(project, n=30){
+    const data = []
+    let i = 1 
+    while (i <= n){
+      const page = await getCnftIoPage(project, i)
+      console.log('cnftIo page '+ i ,page.length)
+      data.push(...page)
+      i++
+    }
+    console.log(`cnftIo ${n} pages: ${data.length} items`)
+    return data
+}
+
+  async function getCnftToolsPage(project, page){
+    const uri = `https://cnft.tools/api/${project}?background=x&body=x&face=x&headwear=x&sort=ASC&method=rarity&page=${page}&`
+    const response = await fetch(uri);
+    const responseData = await response.json();
+    return responseData.stats
+  }
+
+  async function getCnftIoPage(project, page){
 
     const form = {
       search: "",
       sort: "date",
       order: "desc",
-      page: 1,
+      page,
       verified: true,
-      project: "Yummi Universe - Naru",
+      project,
     };
 
     const params = new URLSearchParams(form);
     
 
     const response = await fetch('https://api.cnft.io/market/listings', {method: 'POST', body: params});
-    const data = await response.json()
-    return data
+    const responseData = await response.json()
+    return responseData.assets
   }
-}
-main()
 
 // project list endpoint
 // https://api.cnft.io/market/projects 
@@ -63,26 +122,10 @@ main()
 //
 // pool.io 
 // https://api.cnft.io/market/listings
-// type - post
-// fetch("https://api.cnft.io/market/listings", {
-//   "headers": {
-//     "accept": "*/*",
-//     "accept-language": "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7",
-//     "content-type": "application/x-www-form-urlencoded;charset=UTF-8",
-//     "sec-fetch-dest": "empty",
-//     "sec-fetch-mode": "cors",
-//     "sec-fetch-site": "same-site",
-//     "sec-gpc": "1"
-//   },
-//   "referrer": "https://cnft.io/",
-//   "referrerPolicy": "strict-origin-when-cross-origin",
-//   "body": "search=&sort=date&order=desc&page=1&verified=true&project=Yummi+Universe+-+Naru",
-//   "method": "POST",
-//   "mode": "cors"
-// });
 //
-//
-//
-// checar manualmente se o hash como id da certo
 // error handle nos fetch
-// pensar numa organizaçao melhor
+// mudar o numero de paginas hardcoded em 5
+// otmimizar a comparacao dos 2 dicionarios
+// ver o "available" e colocar nos dados
+// descobrir numero exato de paginas ou ver q q acontece no vim
+//
